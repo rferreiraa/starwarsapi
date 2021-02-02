c28667a1b2782a630042f159e832e53b3fbd6fe4
Rodrigo <rferreiraa02@gmail.com>
2021-02-02 19:42:45 -0300

    initial commit

---
+++ src/main/java/b2w/com/br/starwarsapi/StarwarsapiApplication.java
@@ -0,0 +1,13 @@
+package b2w.com.br.starwarsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/b2w/com/br/starwarsapi/StarwarsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package b2w.com.br.starwarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
